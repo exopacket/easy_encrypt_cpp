720f8d38b8cd6bb4709fe01c2bd8db899ce14777
Ryan Fitzgerald <101024833+inteliense@users.noreply.github.com>
2022-06-07 13:58:23 -0400

    Update aes_gcm.cpp

--- examples/aes_gcm.cpp
+++ examples/aes_gcm.cpp
@@ -11,13 +11,13 @@ int main() {
     std::string hex_key = EasyEncrypt::Random::secureEncoded(EasyEncrypt::HEX, 32);
     std::string hex_iv = EasyEncrypt::Random::secureEncoded(EasyEncrypt::HEX, 16);
 
-    //Getting some Additional Authenticated Data (random bytes)
+    //Getting some Additional Authenticated Data (random bytes) [optional]
     std::string hex_aad = EasyEncrypt::Random::secureEncoded(EasyEncrypt::HEX, 16);
 
     //GCM encoded Encryption/Decryption requires the use of
     //the AESData class
 
-    //Creating an AESData object using the constructor
+    //Creating an AESData object using the constructor that has an aad parameter
     //algorithm_t algorithm, encode_t encoding, std::string key, std::string iv, std::string aad, bool encrypt
     EasyEncrypt::AESData *aes_obj1 = new EasyEncrypt::AESData(EasyEncrypt::algorithm_t::GCM, EasyEncrypt::encode_t::HEX,
                                                               hex_key, hex_iv, hex_aad, true);
@@ -31,6 +31,9 @@ int main() {
     //Printing the encrypted result encoded as a hex string
     std::cout << "Encrypted Result 1: " << aes_obj1->encrypted << std::endl;
 
+    //Printing the encrypted result encoded as a hex string
+    std::cout << "Tag Value: " << aes_obj1->tag << std::endl;
+
     //Setting the do_encrypt variable to false
     aes_obj1->do_encrypt = false;
 
@@ -40,13 +43,19 @@ int main() {
     //Printing the encrypted result encoded as a hex string
     std::cout << "Decrypted Result 1: " << aes_obj1->plaintext << std::endl;
 
+    //checking to see if tag is verified
+    std::cout << "Tag Verifies: " << ((aes_obj1->tag_verifies) ? "Yes" : "No") << std::endl;
+
     /*
      * --------------------------------------------------------------------------------
      */
 
     //Additional Authenticated Data is optional
+    //This example uses none by setting the aad to an empty string
+    //alternatively, you can call the constructor that doesn't have an aad parameter.
 
     std::string input = "My Other Encrypted Message";
+
     //Creating an AESData object using the gcm_gcm_hex_create;
     //std::string input, std::string key, std::string iv,  std::string aad, bool encrypt
     EasyEncrypt::AESData* aes_obj2 = EasyEncrypt::AESData::gcm_hex_create(input, hex_key,
@@ -66,6 +75,9 @@ int main() {
 
     //Printing the encrypted result encoded as a hex string
     std::cout << "Decrypted Result 2 " << gcm_res.plaintext << std::endl;
+
+    //checking to see if tag is verified
+    std::cout << "Tag Verifies: " << ((gcm_res.tag_verifies) ? "Yes" : "No") << std::endl;
 
     return 0;
 
